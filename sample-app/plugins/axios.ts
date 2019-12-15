902cac71d4393a1dd9eb49aaf222444debcd38e2
misaosyushi <misaki.tsujita@rakus-partners.co.jp>
2019-12-15 23:09:57 +0900

    baseURL追加

--- sample-app/plugins/axios.ts
+++ sample-app/plugins/axios.ts
@@ -1,5 +1,5 @@
 export default function({ $axios }) {
   $axios.onRequest((config) => {
-    config.baseURL = process.env.API_BASE_URL
+    config.baseURL = 'https://x0tqbv0o90.execute-api.ap-northeast-1.amazonaws.com/pro'
   })
 }
